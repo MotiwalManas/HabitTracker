6fac5c6813c284b67805715fa3a731ea565e5273
Manas <123235470+MotiwalManas@users.noreply.github.com>
2023-05-31 22:58:03 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Habits from "./Habits";
+const Home = () => {
+  return (
+    <>
+      <Navbar name="Deatil view" />
+      <Habits />
+    </>
+  );
+};
+
+export default Home;
